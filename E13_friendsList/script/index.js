2bda792a2c3db93d24de61cb131997713fa26afc
thanikasomasundaram-t <t.thillaikumar@cdw.com>
2023-03-02 17:41:40 +0530

    Rendered using fragment.

--- E13_friendsList/script/index.js
+++ E13_friendsList/script/index.js
@@ -1,36 +1,37 @@
+// GET CLASS LIST FROM DOM
+const list = document.querySelector('.list');
+const fragment = new DocumentFragment();
 
 for(userDetail of data) {
-    // GET CLASS LIST FROM DOM
-    let list = document.querySelector('.list');
 
     // CREATE CARD ELEMENT
-    let card = document.createElement('div');
+    const card = document.createElement('div');
     card.className = 'card';
 
     // CREATE PROFILE ELEMENT
-    let profile = document.createElement('div');
+    const profile = document.createElement('div');
     profile.className = 'profile';
     
     // CREATE IMG ELEMENT
-    let img = document.createElement('img');
-    let imgLink = userDetail.img;
+    const img = document.createElement('img');
+    const imgLink = userDetail.img;
     img.setAttribute('src', imgLink);
 
     // APPEND IMG TO PROFILE
     profile.appendChild(img);
 
     // CREATE DETAILS ELEMENT
-    let details = document.createElement('div');
+    const details = document.createElement('div');
     details.className = 'details';
     
     // CREATE NAME ELEMENT 
-    let userName = document.createElement('div');
+    const userName = document.createElement('div');
     userName.className = 'name';
-    let userFullName = userDetail.first_name + ' ' + userDetail.last_name;
+    const userFullName = userDetail.first_name + ' ' + userDetail.last_name;
     userName.innerHTML = userFullName;
 
     // CREATE EMAIL ELEMENT
-    let email = document.createElement('div');
+    const email = document.createElement('div');
     email.className = 'email';
     email.innerHTML = userDetail.email;
 
@@ -42,10 +43,12 @@ for(userDetail of data) {
     card.appendChild(profile);
     card.appendChild(details);
 
-    // APPPEND CARD TO LIST
-    list.appendChild(card);
-
+    // APPPEND CARD TO FRAGMENT
+    fragment.appendChild(card);
 }
+
+list.appendChild(fragment);
+
 
 
 
