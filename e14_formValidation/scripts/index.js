7e91076700b1d91f7cc5bf377e369bda9042908e
thanikasomasundaram-t <t.thillaikumar@cdw.com>
2023-03-05 17:43:51 +0530

    Validated using regex.

---
+++ e14_formValidation/scripts/index.js
@@ -0,0 +1,54 @@
+const validateNotEmpty = (element) => {
+    if(/^.{1,}$/.test(element.value) == false) {
+        element.nextElementSibling.innerHTML = element.previousElementSibling.innerHTML + ' is required';
+        element.style.borderColor = '#A80000';
+        
+        return 0;
+    }
+    else {
+        element.style.borderColor = '#c2c2c2';
+        element.nextElementSibling.innerHTML = '';
+        return 1;
+    }
+}
+
+
+validateDetails = (element, regex) => {
+    if(/^.{1,}$/.test(element.value) == false) {
+        element.nextElementSibling.innerHTML = element.previousElementSibling.innerHTML + ' is required';
+        element.style.borderColor = '#A80000';
+        
+        return 0;
+    }
+    if(regex.test(element.value) == false) {
+        element.nextElementSibling.innerHTML = element.previousElementSibling.innerHTML + ' is not valid';
+        element.style.borderColor = '#A80000';
+        return 0;
+    }
+    element.style.borderColor = '#c2c2c2';
+    element.nextElementSibling.innerHTML = '';
+    return 1;
+}
+
+
+const regexValues = {
+    firstName: /^\w+$/,
+    lastName: /^\w+$/,
+    email: /^(\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})){1,50}$/,
+    contactNumber: /^\d{2}\+[0-9]{10}$/,
+    pin: /^\d{6}$/,
+    cardNumber: /^4[0-9]{12}(?:[0-9]{3})?$/,
+    cardExpiry: /20[2-9][0-9]/,
+    cvv: /\d{3,4}/,
+}
+
+
+document.querySelector('button').addEventListener('click', (event) => {
+    event.preventDefault();
+    const form = event.target.parentElement;
+    const inputFields = form.querySelectorAll('input');
+
+    inputFields.forEach(element => {
+            validateDetails(element, regexValues[element.id]);
+    });
+});
